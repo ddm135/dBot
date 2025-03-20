bc30dc29d564ed81bc59f3f129bb7f1e7265c4b6
ddm135 | Aut <ddm135@outlook.com>
2025-03-20 16:14:53 +0700

    new exception type?

--- app_commands/ssLeague.py
+++ app_commands/ssLeague.py
@@ -244,7 +244,7 @@ class SSLeague(commands.GroupCog, name="ssl", description="Pin SSL song of the d
             ) as r:
                 try:
                     ajs = await r.json(content_type=None)
-                except json.JSONDecodeError:
+                except (json.JSONDecodeError, json.decoder.JSONDecodeError):
                     ajs = json.loads(decrypt_cbc(await r.text()))
         return ajs
 
