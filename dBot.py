052d1e7e3b12bd35111b2af5417a630e5173c309
ddm135 | Aut <ddm135@outlook.com>
2025-03-24 18:10:34 +0700

    help?

--- dBot.py
+++ dBot.py
@@ -35,7 +35,7 @@ class dBot(commands.Bot):
         if PING_DATA.exists():
             with open(PING_DATA, "r") as f:
                 self.pings = json.load(
-                    f, object_pairs_hook=partial(defaultdict, lambda: None)
+                    f, object_pairs_hook=partial(defaultdict, lambda: defaultdict(dict))
                 )
 
         else:
