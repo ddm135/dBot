a8ada4cd7ddd0ae690a1ad532d280d619040c549
ddm135 | Aut <ddm135@outlook.com>
2025-11-19 14:40:36 +0700

    is chunk size the problem

--- tasks/basic_sync/basic_sync.py
+++ tasks/basic_sync/basic_sync.py
@@ -106,9 +106,9 @@ class BasicSync(commands.Cog):
                                     continue
                             except UnicodeDecodeError:
                                 pass
-                            # async for chunk in r.content.iter_chunked(CHUNK_SIZE):
-                            #     f.write(chunk)
-                            f.write(await r.read())
+                            async for chunk in r.content.iter_chunked(CHUNK_SIZE):
+                                f.write(chunk)
+                            # f.write(await r.read())
 
                     process = await asyncio.create_subprocess_exec(
                         f"utils/catalog-{extension}",
