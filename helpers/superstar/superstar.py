302c393edf9689337e469d0d6c03843b6dd99d05
ddm135 | Aut <ddm135@outlook.com>
2025-12-12 03:04:36 +0700

    fix logic

--- helpers/superstar/superstar.py
+++ helpers/superstar/superstar.py
@@ -405,10 +405,8 @@ class SuperStar(commands.Cog):
 
             embeds = pin.embeds
             if embeds and (
-                embeds[0].title
-                and embed_title in embeds[0].title
-                or embeds[0].author.name
-                and embed_title in embeds[0].author.name
+                (embeds[0].title and embed_title in embeds[0].title)
+                or (embeds[0].author.name and embed_title in embeds[0].author.name)
             ):
                 await pin.unpin()
                 break
