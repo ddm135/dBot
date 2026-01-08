1443fe8ef3dfa51c4227b0999e25fd72d426fb61
ddm135 | Aut <ddm135@outlook.com>
2026-01-08 23:45:10 +0700

    Revert "seems to be problem with ipv6?"
    This reverts commit 8bab4e1ae156f76a714e1f1be121874f11172f23.

--- tasks/basic_sync/basic_sync.py
+++ tasks/basic_sync/basic_sync.py
@@ -3,7 +3,6 @@
 import asyncio
 import json
 import logging
-import socket
 from datetime import time
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -43,9 +42,7 @@ class BasicSync(commands.Cog):
         self.LOGGER.info("Downloading basic data: %s...", game_details["name"])
         cog: "SuperStar" = self.bot.get_cog("SuperStar")  # type: ignore[assignment]
 
-        async with aiohttp.ClientSession(
-            connector=aiohttp.TCPConnector(family=socket.AF_INET)
-        ) as session:
+        async with aiohttp.ClientSession() as session:
             if {"iconUrl", "lastVersion"} <= set(game_details):
                 version = game_details["lastVersion"]
                 iconUrl = game_details["iconUrl"]
